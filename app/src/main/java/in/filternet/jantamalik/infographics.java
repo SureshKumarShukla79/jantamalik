93858c8e6828d54c5f8dd7993d4603ef64e5daeb
suresh <suresh@filternet.in>
2019-01-24 13:23:05 +0530

    Bugfix - Vote/MP screen layout problem fixed. Inforgraphics title change.

--- app/src/main/java/in/filternet/jantamalik/infographics.java
+++ app/src/main/java/in/filternet/jantamalik/infographics.java
@@ -1,15 +1,28 @@
 package in.filternet.jantamalik;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import static in.filternet.jantamalik.MainActivity.sLANGUAGE_HINDI;
 
 public class infographics extends AppCompatActivity {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences mSharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String mLanguage = mSharedPref.getString(MainActivity.sUSER_CURRENT_LANGUAGE, sLANGUAGE_HINDI);
+        if (mLanguage.equals(sLANGUAGE_HINDI)) {
+            MainActivity.setUI_Lang(this, "hi");
+        }
         setContentView(R.layout.infographics_layout);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setTitle(R.string.democracy);
     }
 }
