2f8adfc396c0c58e70c5c9fc4ddcb15c12a8c7ac
suresh <suresh@filternet.in>
2019-01-25 19:02:09 +0530

    Minor UI fixes

--- app/src/main/java/in/filternet/jantamalik/IssuesJava/IssuesData.java
+++ app/src/main/java/in/filternet/jantamalik/IssuesJava/IssuesData.java
@@ -4,23 +4,13 @@ public class IssuesData {
     public static final String[][] issues = {
             {
                 "Saansad/MP doesn't respond", //title
-                "Call multiple times.\nWrite emails with objective facts and relief needed.\nMeet at home on Sunday 11 am.",
+                "Call multiple times.\nWrite emails with objective facts and relief needed.\nMeet at their home on Sunday 11 am.",
                 "No law for this",
                 "No law for this",
                 "सांसद मेरी बात नहीं सुनता",    //title-hi
-                "सांसद को फोन करें। ईमेल करें।\nघर पर रविवार 11 बजे मिलें।",
+                "सांसद को फोन करें। ईमेल करें।\nउसके घर पर रविवार 11 बजे मिलें।",
                 "कोई कानून नहीं",
                 "कोई कानून नहीं"
-            },
-            {
-                "Unemployment",//title
-                "Unemployment janta views",
-                "Unemployment Loksabha views",
-                "Unemployment Govt views",
-                "बेरोजगारी",      //title-hi
-                "बेरोजगारी जनता के विचार",
-                "बेरोजगारी लोकसभा के विचार",
-                "बेरोजगारी सरकार के विचार",
             }
     };
 }
