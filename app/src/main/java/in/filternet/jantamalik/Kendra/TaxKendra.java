35dd64d24064e41c0a7db5c8a4393640a0b745e5
suresh <suresh@filternet.in>
2019-02-01 18:59:59 +0530

    Keeping the flame icon for 🔥 for Android 6 and above

--- app/src/main/java/in/filternet/jantamalik/Kendra/TaxKendra.java
+++ app/src/main/java/in/filternet/jantamalik/Kendra/TaxKendra.java
@@ -3,12 +3,14 @@ package in.filternet.jantamalik.Kendra;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import in.filternet.jantamalik.MainActivity;
 import in.filternet.jantamalik.R;
@@ -19,6 +21,7 @@ public class TaxKendra extends AppCompatActivity {
 
     private SharedPreferences mSharedPref;
     private Toolbar toolbar;
+    private TextView interest, education, health;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,6 +37,16 @@ public class TaxKendra extends AppCompatActivity {
         setContentView(R.layout.tax_kendra);
 
         toolbar = findViewById(R.id.toolbar_Tax_Kendra);
+        interest = findViewById(R.id.interest);
+        education = findViewById(R.id.education);
+        health = findViewById(R.id.health);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            interest.setText(getString(R.string.interest) + " \uD83D\uDD25\uD83D\uDD25");
+            education.setText(getString(R.string.education) + " \uD83D\uDD25");
+            health.setText(getString(R.string.health) + " \uD83D\uDD25");
+        }
+
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
