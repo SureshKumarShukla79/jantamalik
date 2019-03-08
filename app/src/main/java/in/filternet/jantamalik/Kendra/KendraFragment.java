f68316006f2094d433e74a935bd6d1b653dcef3a
suresh <suresh@filternet.in>
2019-03-08 14:05:58 +0530

    Moving MP selection to My MP screen. Some users fumbled on main screen. This is more localised.

--- app/src/main/java/in/filternet/jantamalik/Kendra/KendraFragment.java
+++ app/src/main/java/in/filternet/jantamalik/Kendra/KendraFragment.java
@@ -9,12 +9,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import in.filternet.jantamalik.MainActivity;
@@ -32,45 +29,18 @@ public class KendraFragment extends Fragment {
     private LinearLayout duties;
 
     private Intent intent;
-    private Spinner spinnerState;
-    private Spinner spinnerMP;
-    private ArrayAdapter arrayAdapterState;
-    private ArrayAdapter arrayAdapterMP;
-
-    private DataFilter dataFilter;
 
     private SharedPreferences mSharedPref;
-    private SharedPreferences.Editor editor;
-
-    public static final String DEFAULT_STATE = "Uttar Pradesh";
-    public static final String DEFAULT_MP = "Varanasi";
-    public static final String DEFAULT_MLA = "Varanasi Cantt";
-    public static final String DEFAULT_WARD = "Chittupur, Sigra";
-
-    public static final String hiDEFAULT_STATE = "उत्तर प्रदेश";
-    public static final String hiDEFAULT_MP = "वाराणसी";
-    public static final String hiDEFAULT_MLA = "वाराणसी कैंट";
-    public static final String hiDEFAULT_WARD = "छित्तुपुर, सिगरा";
-
-    public static final String sSTATE = DEFAULT_STATE;
-    public static final String sMP = DEFAULT_MP;
-    public static final String sMLA = DEFAULT_MLA;
-    public static final String sWARD = DEFAULT_WARD;
-
-    private String mLanguage;
-    public String AreaName;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, final Bundle savedInstanceState) {
         mSharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
-        mLanguage = mSharedPref.getString(MainActivity.sUSER_CURRENT_LANGUAGE, sLANGUAGE_HINDI);
+        String mLanguage = mSharedPref.getString(MainActivity.sUSER_CURRENT_LANGUAGE, sLANGUAGE_HINDI);
         if (mLanguage.equals(sLANGUAGE_HINDI)) {
             MainActivity.setUI_Lang(getActivity(), "hi");
         }
-
-        editor = mSharedPref.edit();
 
         view = inflater.inflate(R.layout.kendra, container, false);
 
@@ -86,94 +56,6 @@ public class KendraFragment extends Fragment {
         govt2 = view.findViewById(R.id.govt2);
 
         duties = view.findViewById(R.id.duties);
-
-        spinnerState = view.findViewById(R.id.state_spinner);
-        spinnerMP = view.findViewById(R.id.MP_spinner);
-
-        // Populating GUI
-        dataFilter = new DataFilter();
-
-        String State = mSharedPref.getString(sSTATE,DEFAULT_STATE);
-        String MP = mSharedPref.getString(sMP,DEFAULT_MP);
-        String MLA = mSharedPref.getString(sMLA,DEFAULT_MLA);
-        String Ward = mSharedPref.getString(sWARD,DEFAULT_WARD);
-        //Log.e(TAG, "state : " + State + " " + MP + " " + MLA + " " + Ward);
-
-        // In case of Hindi, change the defaults
-        if (mLanguage.equals(sLANGUAGE_HINDI)) {
-            if(State.equals(DEFAULT_STATE))
-                State = hiDEFAULT_STATE;
-            if(MP.equals(DEFAULT_MP))
-                MP = hiDEFAULT_MP;
-            if(MLA.equals(DEFAULT_MLA))
-                MLA = hiDEFAULT_MLA;
-            if(Ward.equals(DEFAULT_WARD))
-                Ward = hiDEFAULT_WARD;
-        }
-        //Log.e(TAG, "state : " + State + " " + MP + " " + MLA + " " + Ward);
-
-        // In case the db isn't initialised, do it now
-        editor.putString(sSTATE, State).commit();
-        editor.putString(sMP, MP).commit();
-        editor.putString(sMLA, MLA).commit();
-        editor.putString(sWARD, Ward).commit();
-
-        // Load defaults
-        arrayAdapterState = new ArrayAdapter(view.getContext(), android.R.layout.simple_spinner_item,
-                dataFilter.getStates(mLanguage));
-        arrayAdapterState.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerState.setAdapter(arrayAdapterState);
-
-        int spinnerPosition = arrayAdapterState.getPosition(State);
-        spinnerState.setSelection(spinnerPosition);
-        //Log.e(TAG, "state def: " + State);
-
-        //populating MP Area
-        arrayAdapterMP = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_item,
-                dataFilter.getMPAreas(mLanguage,State));
-        arrayAdapterMP.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerMP.setAdapter(arrayAdapterMP);
-
-        spinnerPosition = arrayAdapterMP.getPosition(MP);
-        spinnerMP.setSelection(spinnerPosition);
-        //Log.e(TAG, "MP def: " + MP);
-        // defaults over
-
-        // set for dynamic handling
-        spinnerState.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String State = spinnerState.getItemAtPosition(spinnerState.getSelectedItemPosition()).toString();
-               // Log.e(TAG, "spin state : " + i + " " + l + " " + State);
-                editor.putString(sSTATE, State).commit();
-
-                // Reload the state MP areas
-                arrayAdapterMP = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_item,
-                        dataFilter.getMPAreas(mLanguage,State));
-                arrayAdapterMP.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spinnerMP.setAdapter(arrayAdapterMP);
-
-                String MP = mSharedPref.getString(sMP,DEFAULT_MP);
-                int spinnerPosition = arrayAdapterMP.getPosition(MP);
-                spinnerMP.setSelection(spinnerPosition);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {}
-        });
-
-        //spinner constituency click handler
-        spinnerMP.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                AreaName = adapterView.getItemAtPosition(i).toString();
-                //Log.e(TAG, "spin MP : " + i + " " + l + " " + AreaName);
-                editor.putString(sMP, AreaName ).commit();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {}
-        });
 
         vote_Click();
         note_Click();
--- app/src/main/java/in/filternet/jantamalik/Kendra/VoteMP.java
+++ app/src/main/java/in/filternet/jantamalik/Kendra/VoteMP.java
@@ -9,6 +9,9 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,9 +22,6 @@ import in.filternet.jantamalik.R;
 import static in.filternet.jantamalik.MainActivity.TAB_KENDRA;
 import static in.filternet.jantamalik.MainActivity.TAB_NUMBER;
 import static in.filternet.jantamalik.MainActivity.sLANGUAGE_HINDI;
-import static in.filternet.jantamalik.VoteJava.VoteFragment.DEFAULT_MP;
-import static in.filternet.jantamalik.VoteJava.VoteFragment.hiDEFAULT_MP;
-import static in.filternet.jantamalik.VoteJava.VoteFragment.sMP;
 
 public class VoteMP extends AppCompatActivity {
     String TAG = "VoteMP";
@@ -30,6 +30,33 @@ public class VoteMP extends AppCompatActivity {
     private de.hdodenhof.circleimageview.CircleImageView profile_pic;
     DataFilter.MP_info mp;
 
+    private Spinner spinnerState;
+    private Spinner spinnerMP;
+    private ArrayAdapter arrayAdapterState;
+    private ArrayAdapter arrayAdapterMP;
+    private DataFilter dataFilter;
+
+    private SharedPreferences mSharedPref;
+    private SharedPreferences.Editor editor;
+
+    public static final String DEFAULT_STATE = "Uttar Pradesh";
+    public static final String DEFAULT_MP = "Varanasi";
+    public static final String DEFAULT_MLA = "Varanasi Cantt";
+    public static final String DEFAULT_WARD = "Chittupur, Sigra";
+
+    public static final String hiDEFAULT_STATE = "उत्तर प्रदेश";
+    public static final String hiDEFAULT_MP = "वाराणसी";
+    public static final String hiDEFAULT_MLA = "वाराणसी कैंट";
+    public static final String hiDEFAULT_WARD = "छित्तुपुर, सिगरा";
+
+    public static final String sSTATE = DEFAULT_STATE;
+    public static final String sMP = DEFAULT_MP;
+    public static final String sMLA = DEFAULT_MLA;
+    public static final String sWARD = DEFAULT_WARD;
+
+    private String MPArea;
+    private String mLanguage;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
@@ -37,12 +64,18 @@ public class VoteMP extends AppCompatActivity {
 
         setContentView(R.layout.vote_mp_layout);
 
+        mSharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = mSharedPref.edit();
+
         toolbar = findViewById(R.id.toolbar_MP_layout);
         name = findViewById(R.id.MP_name);
         phone = findViewById(R.id.phone);
         email = findViewById(R.id.email);
         address = findViewById(R.id.address);
         profile_pic = findViewById(R.id.profile_image);
+
+        spinnerState = findViewById(R.id.state_spinner);
+        spinnerMP = findViewById(R.id.MP_spinner);
 
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -52,22 +85,114 @@ public class VoteMP extends AppCompatActivity {
             }
         });
 
-        SharedPreferences mSharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-        String MPArea = mSharedPref.getString(sMP,DEFAULT_MP);
-        String current_language = mSharedPref.getString(MainActivity.sUSER_CURRENT_LANGUAGE, sLANGUAGE_HINDI);
+        final SharedPreferences mSharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        MPArea = mSharedPref.getString(sMP, DEFAULT_MP);
+        mLanguage = mSharedPref.getString(MainActivity.sUSER_CURRENT_LANGUAGE, sLANGUAGE_HINDI);
 
+        String State = mSharedPref.getString(sSTATE, DEFAULT_STATE);
+        String MP = mSharedPref.getString(sMP, DEFAULT_MP);
+        String MLA = mSharedPref.getString(sMLA, DEFAULT_MLA);
+        String Ward = mSharedPref.getString(sWARD, DEFAULT_WARD);
+        //Log.e(TAG, "state : " + State + " " + MP + " " + MLA + " " + Ward);
+
+        // In case of Hindi, change the defaults
+        if (mLanguage.equals(sLANGUAGE_HINDI)) {
+            if (State.equals(DEFAULT_STATE))
+                State = hiDEFAULT_STATE;
+            if (MP.equals(DEFAULT_MP))
+                MP = hiDEFAULT_MP;
+            if (MLA.equals(DEFAULT_MLA))
+                MLA = hiDEFAULT_MLA;
+            if (Ward.equals(DEFAULT_WARD))
+                Ward = hiDEFAULT_WARD;
+        }
+        //Log.e(TAG, "state : " + State + " " + MP + " " + MLA + " " + Ward);
+
+        // In case the db isn't initialised, do it now
+        editor.putString(sSTATE, State).commit();
+        editor.putString(sMP, MP).commit();
+        editor.putString(sMLA, MLA).commit();
+        editor.putString(sWARD, Ward).commit();
+
+        // Populating GUI
+        dataFilter = new DataFilter();
+        // Load defaults
+        arrayAdapterState = new ArrayAdapter(getBaseContext(), android.R.layout.simple_spinner_item,
+                dataFilter.getStates(mLanguage));
+        arrayAdapterState.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerState.setAdapter(arrayAdapterState);
+
+        int spinnerPosition = arrayAdapterState.getPosition(State);
+        spinnerState.setSelection(spinnerPosition);
+        //Log.e(TAG, "state def: " + State);
+
+        //populating MP Area
+        arrayAdapterMP = new ArrayAdapter(getBaseContext(), android.R.layout.simple_spinner_item,
+                dataFilter.getMPAreas(mLanguage, State));
+        arrayAdapterMP.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMP.setAdapter(arrayAdapterMP);
+
+        spinnerPosition = arrayAdapterMP.getPosition(MP);
+        spinnerMP.setSelection(spinnerPosition);
+        //Log.e(TAG, "MP def: " + MP);
+        // defaults over
+
+        // set for dynamic handling
+        spinnerState.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String State = spinnerState.getItemAtPosition(spinnerState.getSelectedItemPosition()).toString();
+                //Log.e(TAG, "spin state : " + i + " " + l + " " + State);
+                editor.putString(sSTATE, State).commit();
+
+                // Reload the state MP areas
+                arrayAdapterMP = new ArrayAdapter(getBaseContext(), android.R.layout.simple_spinner_item,
+                        dataFilter.getMPAreas(mLanguage, State));
+                arrayAdapterMP.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinnerMP.setAdapter(arrayAdapterMP);
+
+                String MP = mSharedPref.getString(sMP, DEFAULT_MP);
+                int spinnerPosition = arrayAdapterMP.getPosition(MP);
+                spinnerMP.setSelection(spinnerPosition);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+        //spinner constituency click handler
+        spinnerMP.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                MPArea = adapterView.getItemAtPosition(i).toString();
+                //Log.e(TAG, "spin MP : " + i + " " + l + " " + MPArea);
+                editor.putString(sMP, MPArea).commit();
+
+                updateMP();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+        updateMP();
+    }
+
+    private void updateMP() {
         DataFilter dataFilter = new DataFilter();
         //mp = dataFilter.new MP_info();
-        mp = dataFilter.getMPInfo(current_language, MPArea);
+        mp = dataFilter.getMPInfo(mLanguage, MPArea);
 
-      //  Log.e(TAG, MPArea + " " + mp.name + " " + mp.phone + " " + mp.email + " " + mp.address);
+        //Log.e(TAG, MPArea + " " + mp.name + " " + mp.phone + " " + mp.email + " " + mp.address);
         name.setText(mp.name);
         phone.setText(mp.phone);
         email.setText(mp.email);
         address.setText(mp.address);
 
         // Only Varanasi MP pic in app
-        if(MPArea.equals(DEFAULT_MP) || MPArea.equals(hiDEFAULT_MP))
+        if (MPArea.equals(DEFAULT_MP) || MPArea.equals(hiDEFAULT_MP))
             profile_pic.setImageResource(R.drawable.narendra_modi_pic);
         else
             profile_pic.setImageResource(R.drawable.politician_illustration);
