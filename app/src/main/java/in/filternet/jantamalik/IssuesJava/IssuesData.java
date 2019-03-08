8618825668dc65748487d37fbdc79da2809fb391
suresh <suresh@filternet.in>
2019-03-08 12:47:34 +0530

    UX = icons for each tab makes more interactive, icon for how voters run govt, minor improvements.

--- app/src/main/java/in/filternet/jantamalik/IssuesJava/IssuesData.java
+++ app/src/main/java/in/filternet/jantamalik/IssuesJava/IssuesData.java
@@ -3,8 +3,7 @@ package in.filternet.jantamalik.IssuesJava;
 public class IssuesData {
     public static final String[][] issues = {
             {
-
-                    "Saansad/MP doesn't respond",   // title
+                    "Saansad/Vidhayak doesn't respond",   // title
                     "Call multiple times.\nWrite emails with objective facts and relief needed.\n\nMeet at their home on Sunday 11 am. This gets quick notice and speedy results.",      // janta ki problem with details
                     "",       // sansad ka behaviour
                     "",       // vidhayak ka behaviour
@@ -13,16 +12,16 @@ public class IssuesData {
                     "",   // center govt "" means NA
                     "",  // state govt "" means NA
                     "",    // municipality response "" means NA
-                    "सांसद मेरी बात नहीं सुनता",    //title Hindi
-                    "सांसद को फोन करें। \nतथ्यों के साथ ईमेल करें।\n\nउसके घर पर रविवार सुबह 11 बजे मिलें। यह जल्दी ध्यान खींचता है और जल्दी समाधान भी देता है।", // janta ki problem with details
+                    "सांसद/विधायक मेरी बात नहीं सुनता",    //title Hindi
+                    "सांसद/विधायक को फोन करें। \nतथ्यों के साथ ईमेल करें।\n\nउसके घर पर रविवार सुबह 11 बजे मिलें। यह जल्दी ध्यान खींचता है और जल्दी समाधान भी देता है।", // janta ki problem with details
                     "",// sansad ka behaviour
                     "",       // vidhayak ka behaviour
                     "",       // parshad ka behaviour
-                    "गैर-जिम्मेदार सांसद को 5 साल पूरा होने से पहले हटाने का कोई कानून नहीं है।\n\nअगली बार ध्यान से चुनें।",// parliament ke law
+                    "गैर-जिम्मेदार सांसदय/विधायक को 5 साल पूरा होने से पहले हटाने का कोई कानून नहीं है।\n\nअगली बार ध्यान से चुनें।",// parliament ke law
                     "",   // center govt "" means NA
                     "",  // state govt "" means NA
                     "",    // municipality response "" means NA
-            }/*,
+            },
             {
                     "Cleanliness", //title
                     "Call multiple times.\nWrite emails with objective facts and relief needed.\nMeet at their home on Sunday 11 am.",      // janta ki problem with details
@@ -33,7 +32,7 @@ public class IssuesData {
                     "No law for this",   // center govt "" means NA
                     "",  // state govt "" means NA
                     "",    // municipality response "" means NA
-                    "सांसद मेरी बात नहीं सुनता",    //title Hindi
+                    "सफाई",    //title Hindi
                     "सांसद को फोन करें। ईमेल करें।\nउसके घर पर रविवार 11 बजे मिलें।", // janta ki problem with details
                     "कोई कानून नहीं",// sansad ka behaviour
                     "",       // vidhayak ka behaviour
@@ -44,7 +43,7 @@ public class IssuesData {
                     "",    // municipality response "" means NA
             },
             {
-                    "Ease-of-doing-business poor", //title
+                    "Difficulty of starting business", //title
                     "Call multiple times.\nWrite emails with objective facts and relief needed.\nMeet at their home on Sunday 11 am.",      // janta ki problem with details
                     "irresponsible time wastage",       // sansad ka behaviour
                     "",       // vidhayak ka behaviour
@@ -53,7 +52,7 @@ public class IssuesData {
                     "No law for this",   // center govt "" means NA
                     "",  // state govt "" means NA
                     "",    // municipality response "" means NA
-                    "सांसद मेरी बात नहीं सुनता",    //title Hindi
+                    "नये उद्योग लगाने की मुश्किलें",    //title Hindi
                     "सांसद को फोन करें। ईमेल करें।\nउसके घर पर रविवार 11 बजे मिलें।", // janta ki problem with details
                     "कोई कानून नहीं",// sansad ka behaviour
                     "",       // vidhayak ka behaviour
@@ -64,7 +63,7 @@ public class IssuesData {
                     "",    // municipality response "" means NA
             },
             {
-                    "Some random Issue",   // title
+                    "Road random Issue",   // title
                     "Call multiple times.\nWrite emails with objective facts and relief needed.\nMeet at their home on Sunday 11 am.",      // janta ki problem with details
                     "irresponsible time wastage",       // sansad ka behaviour
                     "",       // vidhayak ka behaviour
@@ -73,7 +72,7 @@ public class IssuesData {
                     "No law for this",   // center govt "" means NA
                     "",  // state govt "" means NA
                     "",    // municipality response "" means NA
-                    "सांसद मेरी बात नहीं सुनता",    //title Hindi
+                    "सड़क",    //title Hindi
                     "सांसद को फोन करें। ईमेल करें।\nउसके घर पर रविवार 11 बजे मिलें।", // janta ki problem with details
                     "कोई कानून नहीं",// sansad ka behaviour
                     "",       // vidhayak ka behaviour
@@ -82,8 +81,26 @@ public class IssuesData {
                     "कोई कानून नहीं",   // center govt "" means NA
                     "",  // state govt "" means NA
                     "",    // municipality response "" means NA
-            },*/
+            },
+            {
+                    "Inflation",   // title
+                    "Call multiple times.\nWrite emails with objective facts and relief needed.\nMeet at their home on Sunday 11 am.",      // janta ki problem with details
+                    "irresponsible time wastage",       // sansad ka behaviour
+                    "",       // vidhayak ka behaviour
+                    "",       // parshad ka behaviour
+                    "No law for this",   // parliament ke law
+                    "No law for this",   // center govt "" means NA
+                    "",  // state govt "" means NA
+                    "",    // municipality response "" means NA
+                    "मंहगाई",    //title Hindi
+                    "सांसद को फोन करें। ईमेल करें।\nउसके घर पर रविवार 11 बजे मिलें।", // janta ki problem with details
+                    "कोई कानून नहीं",// sansad ka behaviour
+                    "",       // vidhayak ka behaviour
+                    "",       // parshad ka behaviour
+                    "कोई कानून नहीं",// parliament ke law
+                    "कोई कानून नहीं",   // center govt "" means NA
+                    "",  // state govt "" means NA
+                    "",    // municipality response "" means NA
+            },
     };
 }
-
-
