10135bf089557b92269640fa60b7461bfb66aa49
suresh <suresh@filternet.in>
2019-03-09 18:29:55 +0530

    UI fix - kendra back button corrected.

--- app/src/main/java/in/filternet/jantamalik/Kendra/Infographics.java
+++ app/src/main/java/in/filternet/jantamalik/Kendra/Infographics.java
@@ -1,17 +1,24 @@
 package in.filternet.jantamalik.Kendra;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import in.filternet.jantamalik.MainActivity;
 import in.filternet.jantamalik.R;
 
+import static in.filternet.jantamalik.MainActivity.TAB_KENDRA;
+import static in.filternet.jantamalik.MainActivity.TAB_NUMBER;
 import static in.filternet.jantamalik.MainActivity.sLANGUAGE_HINDI;
 
 public class Infographics extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,9 +30,22 @@ public class Infographics extends AppCompatActivity {
         if (mLanguage.equals(sLANGUAGE_HINDI)) {
             MainActivity.setUI_Lang(this, "hi");
         }
-        setContentView(R.layout.infographics_kendra);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        getSupportActionBar().setTitle(R.string.democracy);
+        setContentView(R.layout.infographics_kendra);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                back_button(view);
+            }
+        });
+    }
+
+    private void back_button(View view) {
+        Intent intent = new Intent(view.getContext(), MainActivity.class);
+        intent.putExtra(TAB_NUMBER, TAB_KENDRA);
+        startActivity(intent);
     }
 }
