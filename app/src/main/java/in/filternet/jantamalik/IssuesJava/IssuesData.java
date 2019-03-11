6c85043bd7c48222c276131f7d256dbedfeeb23a
suresh <suresh@filternet.in>
2019-03-11 20:58:43 +0530

    Release 19.03.11 - Why this app? answer. Added Issues, minor improvisations.

--- app/src/main/java/in/filternet/jantamalik/IssuesJava/IssuesData.java
+++ app/src/main/java/in/filternet/jantamalik/IssuesJava/IssuesData.java
@@ -4,8 +4,9 @@ public class IssuesData {
     public static final String[][] issues = {
             {
                     "Saansad/Vidhayak doesn't respond",   // title
-                    "Call multiple times.\nWrite emails with objective facts and relief needed.\n\nMeet at their home on Sunday 11 am. This gets quick notice and speedy results.",      // janta ki problem with details
-                    "",       // sansad ka behaviour
+                    "Call multiple times.\nWrite emails with objective facts and relief needed." +
+                            "\n\nMeet at their home on Sunday 11 am. This gets quick notice and speedy results.",      // janta ki problem with details
+                    "During Anna Hazare fast for Jan Lokpal bill, when people protested outside homes, it worked wonders.",       // sansad ka behaviour
                     "",       // vidhayak ka behaviour
                     "",       // parshad ka behaviour
                     "No law to remove irresponsible saansad before completing 5 year term.\n\nChoose wisely next time.",   // parliament ke law
@@ -13,8 +14,9 @@ public class IssuesData {
                     "",  // state govt "" means NA
                     "",    // municipality response "" means NA
                     "सांसद/विधायक मेरी बात नहीं सुनता",    //title Hindi
-                    "सांसद/विधायक को फोन करें। \nतथ्यों के साथ ईमेल करें।\n\nउसके घर पर रविवार सुबह 11 बजे मिलें। यह जल्दी ध्यान खींचता है और जल्दी समाधान भी देता है।", // janta ki problem with details
-                    "",// sansad ka behaviour
+                    "सांसद/विधायक को फोन करें। \nतथ्यों के साथ ईमेल करें।" +
+                            "\n\nउसके घर पर रविवार सुबह 11 बजे मिलें। यह जल्दी ध्यान खींचता है और जल्दी समाधान भी देता है।", // janta ki problem with details
+                    "अन्ना आंदोलन या जनलोकपाल आंदोलन के समय भी, घर के सामने आंदोलन करने का सबसे तेज असर पड़ा था।",// sansad ka behaviour
                     "",       // vidhayak ka behaviour
                     "",       // parshad ka behaviour
                     "गैर-जिम्मेदार सांसदय/विधायक को 5 साल पूरा होने से पहले हटाने का कोई कानून नहीं है।\n\nअगली बार ध्यान से चुनें।",// parliament ke law
@@ -22,67 +24,7 @@ public class IssuesData {
                     "",  // state govt "" means NA
                     "",    // municipality response "" means NA
             },
-            {
-                    "Cleanliness", //title
-                    "Call multiple times.\nWrite emails with objective facts and relief needed.\nMeet at their home on Sunday 11 am.",      // janta ki problem with details
-                    "irresponsible time wastage",       // sansad ka behaviour
-                    "",       // vidhayak ka behaviour
-                    "",       // parshad ka behaviour
-                    "No law for this",   // parliament ke law
-                    "No law for this",   // center govt "" means NA
-                    "",  // state govt "" means NA
-                    "",    // municipality response "" means NA
-                    "सफाई",    //title Hindi
-                    "सांसद को फोन करें। ईमेल करें।\nउसके घर पर रविवार 11 बजे मिलें।", // janta ki problem with details
-                    "कोई कानून नहीं",// sansad ka behaviour
-                    "",       // vidhayak ka behaviour
-                    "",       // parshad ka behaviour
-                    "कोई कानून नहीं",// parliament ke law
-                    "कोई कानून नहीं",   // center govt "" means NA
-                    "",  // state govt "" means NA
-                    "",    // municipality response "" means NA
-            },
-            {
-                    "Difficulty of starting business", //title
-                    "Call multiple times.\nWrite emails with objective facts and relief needed.\nMeet at their home on Sunday 11 am.",      // janta ki problem with details
-                    "irresponsible time wastage",       // sansad ka behaviour
-                    "",       // vidhayak ka behaviour
-                    "",       // parshad ka behaviour
-                    "No law for this",   // parliament ke law
-                    "No law for this",   // center govt "" means NA
-                    "",  // state govt "" means NA
-                    "",    // municipality response "" means NA
-                    "नये उद्योग लगाने की मुश्किलें",    //title Hindi
-                    "सांसद को फोन करें। ईमेल करें।\nउसके घर पर रविवार 11 बजे मिलें।", // janta ki problem with details
-                    "कोई कानून नहीं",// sansad ka behaviour
-                    "",       // vidhayak ka behaviour
-                    "",       // parshad ka behaviour
-                    "कोई कानून नहीं",// parliament ke law
-                    "कोई कानून नहीं",   // center govt "" means NA
-                    "",  // state govt "" means NA
-                    "",    // municipality response "" means NA
-            },
-            {
-                    "Road random Issue",   // title
-                    "Call multiple times.\nWrite emails with objective facts and relief needed.\nMeet at their home on Sunday 11 am.",      // janta ki problem with details
-                    "irresponsible time wastage",       // sansad ka behaviour
-                    "",       // vidhayak ka behaviour
-                    "",       // parshad ka behaviour
-                    "No law for this",   // parliament ke law
-                    "No law for this",   // center govt "" means NA
-                    "",  // state govt "" means NA
-                    "",    // municipality response "" means NA
-                    "सड़क",    //title Hindi
-                    "सांसद को फोन करें। ईमेल करें।\nउसके घर पर रविवार 11 बजे मिलें।", // janta ki problem with details
-                    "कोई कानून नहीं",// sansad ka behaviour
-                    "",       // vidhayak ka behaviour
-                    "",       // parshad ka behaviour
-                    "कोई कानून नहीं",// parliament ke law
-                    "कोई कानून नहीं",   // center govt "" means NA
-                    "",  // state govt "" means NA
-                    "",    // municipality response "" means NA
-            },
-            {
+            /*{
                     "Inflation",   // title
                     "Call multiple times.\nWrite emails with objective facts and relief needed.\nMeet at their home on Sunday 11 am.",      // janta ki problem with details
                     "irresponsible time wastage",       // sansad ka behaviour
@@ -100,6 +42,88 @@ public class IssuesData {
                     "कोई कानून नहीं",// parliament ke law
                     "कोई कानून नहीं",   // center govt "" means NA
                     "",  // state govt "" means NA
+                    "",    // municipality response "" means NA
+            },*/
+            {
+                    "Cleanliness", //title
+                    "Cleanliness of roads, removing silt and stray animals is responsibility of local municipality.\nIf local municipality does not take care, then state govt is responsible.",      // janta ki problem with details
+                    "",       // sansad ka behaviour
+                    "Its duty of Vidhayak to take up this issue with state govt. Contact Vidhayak(MLA).",       // vidhayak ka behaviour
+                    "",       // parshad ka behaviour
+                    "",   // parliament ke law
+                    "",   // center govt "" means NA
+                    "",  // state govt "" means NA
+                    "",    // municipality response "" means NA
+                    "सफाई",    //title Hindi
+                    "सड़क पर गंदगी, सिल्ट, छुट्टा पशु जैसी समस्याएँ स्थानीय नगर निगम की जिम्मेदारी है।\nअगर नगर निगम अपनी जिम्मेदारी नहीं निभाता तो यह राज्य सरकार की जिम्मेदारी है।", // janta ki problem with details
+                    "",// sansad ka behaviour
+                    "राज्य सरकार में समस्या उठाने की जिम्मेदारी विधायक की है। विधायक को संपर्क करें।",       // vidhayak ka behaviour
+                    "",       // parshad ka behaviour
+                    "",// parliament ke law
+                    "",   // center govt "" means NA
+                    "",  // state govt "" means NA
+                    "",    // municipality response "" means NA
+            },
+            {
+                    "Poor Road",   // title
+                    "Road connectivity and road quality are responsibilities of state govt.",      // janta ki problem with details
+                    "",       // sansad ka behaviour
+                    "Its duty of Vidhayak to take up this issue with state govt. Contact Vidhayak(MLA).",       // vidhayak ka behaviour
+                    "",       // parshad ka behaviour
+                    "",   // parliament ke law
+                    "",   // center govt "" means NA
+                    "",  // state govt "" means NA
+                    "",    // municipality response "" means NA
+                    "सड़क",    //title Hindi
+                    "सड़क बनवाना और उसकी रख-रखाव राज्य सरकार की जिम्मेदारी है।", // janta ki problem with details
+                    "",// sansad ka behaviour
+                    "राज्य सरकार में समस्या उठाने की जिम्मेदारी विधायक की है। विधायक को संपर्क करें।",       // vidhayak ka behaviour
+                    "",       // parshad ka behaviour
+                    "",// parliament ke law
+                    "",   // center govt "" means NA
+                    "",  // state govt "" means NA
+                    "",    // municipality response "" means NA
+            },
+            {
+                    "Electricity",   // title
+                    "Electricity - connection, quality and cost are responsibilities of state govt.",      // janta ki problem with details
+                    "",       // sansad ka behaviour
+                    "Its duty of Vidhayak to take up this issue with state govt. Contact Vidhayak(MLA).",       // vidhayak ka behaviour
+                    "",       // parshad ka behaviour
+                    "",   // parliament ke law
+                    "",   // center govt "" means NA
+                    "",  // state govt "" means NA
+                    "",    // municipality response "" means NA
+                    "बिजली",    //title Hindi
+                    "बिजली कनेक्शन, गुणवत्ता और कीमत - राज्य सरकार की जिम्मेदारी है।", // janta ki problem with details
+                    "",// sansad ka behaviour
+                    "राज्य सरकार में समस्या उठाने की जिम्मेदारी विधायक की है। विधायक को संपर्क करें।",       // vidhayak ka behaviour
+                    "",       // parshad ka behaviour
+                    "",// parliament ke law
+                    "",   // center govt "" means NA
+                    "",  // state govt "" means NA
+                    "",    // municipality response "" means NA
+            },
+            {
+                    "Low ease-of-doing-business", //title
+                    "New businesses are backbone of economy. It creates new jobs.\nOpening a business is very difficult. Complex procedures waste precious time and energy." +
+                            "\n\nWorld ranking of India is low (100th position in 2018). \n\nDue to poor law-n-order, investors hesitate to invest.",      // janta ki problem with details
+                    "irresponsible time wastage",       // sansad ka behaviour
+                    "",       // vidhayak ka behaviour
+                    "",       // parshad ka behaviour
+                    "Making simpler laws is responsibility of Saansad.",   // parliament ke law
+                    "",   // center govt "" means NA
+                    "",  // state govt "" means NA
+                    "Law-n-order in state is responsibility of Vidhayak",    // municipality response "" means NA
+                    "नये उद्योग (स्टार्टप) की मुश्किलें",    //title Hindi
+                    "नये उद्योग अर्थव्यवस्था की रीढ़ होते हैं। नये रोजगार को जन्म देते हैं।\nदेश में रोजगार खोलना काफी मुश्किल है। कई झंझट भरे नियम ध्यान को बाँटते हैं और कीमती समय बर्बाद कर देते हैं।" +
+                            "\n\nविश्व में भारत का स्थान काफी नीचे है (2018 में 100 वां स्थान)। \n\nखराब कानून व्यवस्था के कारण पूंजी लगाने से लोग हिचकते हैं।", // janta ki problem with details
+                    "",// sansad ka behaviour
+                    "",       // vidhayak ka behaviour
+                    "",       // parshad ka behaviour
+                    "कानून को आसान बनाने की जिम्मेदारी सांसदों की है। सांसद को संपर्क करें।",// parliament ke law
+                    "",   // center govt "" means NA
+                    "राज्य में कानून व्यवस्था की जिम्मेदारी विधायकों की है। विधायक को संपर्क करें।",  // state govt "" means NA
                     "",    // municipality response "" means NA
             },
     };
