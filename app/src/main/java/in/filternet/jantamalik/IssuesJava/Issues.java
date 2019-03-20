bb4abff3171f61543cf0485ce58854b4c9820350
monika <monika.bose@filternet.in>
2019-03-20 15:08:53 +0530

    Release 19.03.20

--- app/src/main/java/in/filternet/jantamalik/IssuesJava/Issues.java
+++ app/src/main/java/in/filternet/jantamalik/IssuesJava/Issues.java
@@ -1,23 +1,18 @@
 package in.filternet.jantamalik.IssuesJava;
 
-import android.content.SharedPreferences;
-import android.os.Build;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.transition.Explode;
-import android.transition.Fade;
 import android.util.Log;
-import android.view.Window;
+import android.view.View;
+import android.widget.Toast;
 
-import in.filternet.jantamalik.MainActivity;
+import in.filternet.jantamalik.R;
 
 public class Issues extends AppCompatActivity {
-    private final static String TAG ="MainActivity";
-
-    private String current_language;
-    private SharedPreferences mSharedPref;
+    private final static String TAG ="Issues";
 
     private int layoutResID, titleID;
 
@@ -33,16 +28,30 @@ public class Issues extends AppCompatActivity {
             //Log.e(TAG, "title_id: " + titleID);
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
-            getWindow().setEnterTransition(new Explode());
-            getWindow().setExitTransition(new Fade());
-        }
-        mSharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-
-        current_language = mSharedPref.getString(MainActivity.sUSER_CURRENT_LANGUAGE, MainActivity.sLANGUAGE_HINDI);
-
         setContentView(layoutResID);
         this.setTitle(titleID);
+    }
+
+    public void onclick_update_issue(View view) {
+        String[] TO = {getString(R.string.support_email)};
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        intent.setType("text/plain");
+
+        intent.setPackage("com.google.android.gm");
+        intent.putExtra(Intent.EXTRA_EMAIL, TO);
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(titleID));
+        try {
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                //Log.e(TAG, "1st option");
+                startActivity(intent);
+            } else {
+                //Log.e(TAG, "2nd option");
+                startActivity(Intent.createChooser(intent, "Sending mail..."));
+                finish();
+            }
+        } catch (Exception ex) {
+            Toast.makeText(this, "Gmail app didn't respond.", Toast.LENGTH_LONG).show();
+        }
     }
 }
