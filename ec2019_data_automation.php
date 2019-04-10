5f2a3e02737ab6790adccb550b8d3c7e0186a1b0
suresh <suresh@filternet.in>
2019-04-10 16:06:34 +0530

    ADR json import into database. We need this for filtering and good/bad bucket separation and adding Hindi + research.

---
+++ ec2019_data_automation.php
@@ -0,0 +1,11 @@
+// get the list of seat/ constituencies
+
+// for every seat, if json not present then pull myneta URLs for json
+
+// save the json
+
+// parse the json for candidate details - filter them into good/bad buckets (arrays)
+
+// for good and bad array - translate using Google APIs
+
+// final output is file 
