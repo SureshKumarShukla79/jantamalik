be2206971b8ec851f46b6801887d90c3e8ea941c
monika <monika.bose@filternet.in>
2020-07-13 12:09:57 +0530

    Mapping MP to MLA

--- app/src/main/java/in/filternet/jantamalik/Rajya/MP_2_MLA.java
+++ app/src/main/java/in/filternet/jantamalik/Rajya/MP_2_MLA.java
@@ -4,16 +4,28 @@ public final class MP_2_MLA {
 
     public static final String[][] mapping = {
 
-        //  1               2                   3                4              5                  6                    7               8                   9                   10                  11                 12
-        {"Agra",        "Etmadpur",     "Agra Cantt. (sc)", "Agra South",   "Agra North",   "Agra Rural (sc)",  "Fatehpur Sikri",   "Kheragarh",        "Fatehabad",        "Bah"},
-        {"आगरा",        "एतमादपुर",       "आगरा कैंट. (एस सी)", "आगरा दक्षिण",   "आगरा उत्तर",      "आगरा ग्रामीण (एस सी)", "फतेहपुर सीकरी",     "खेरागढ़",             "फतेहाबाद",          "बाह"},
-        {"Aligarh",     "Khair (sc)",   "Barauli",          "Atrauli",      "Chharra",      "Koil",             "Aligarh",          "Iglas (sc)"},
-        {"अलीगढ़",       "खैर (एस सी)",   "बरौली",              "अतरौली",       "छर्रा",           "कोइल",             "अलीगढ़",           "इगलास (एस सी)"},
-        {"Allahabad",   "Phaphamau",    "Soraon (sc)",      "Phulpur",      "Pratappur",    "Handia",           "Meja",             "Karachhana",       "Allahabad West",   "Allahabad North", "Allahabad South",   "Bara (sc)",     "Koraon (sc)"},
-        {"इलाहाबाद",     "फाफामऊ",        "सोराअों (एस सी)",    "फूलपुर",         "प्रतापपुर",        "हंडिया",             "मेजा",             "करचना",            "इलाहाबाद पश्चिम",      "इलाहाबाद उत्तर",       "इलाहाबाद दक्षिण",      "बारा (एस सी)",     "कोरों (एस सी)"},
-        {"Mirzapur",    "Chhanbey (sc)",     "Mirzapur",         "Majhawan",     "Chunar",       "Marihan"},
-        {"मिर्जापुर",       "छानबे (एस सी)",          "मिर्जापुर",            "मझवां",          "चुनार",         "मड़िहान"},
-        {"Varanasi",    "Pindra",       "Ajagara (sc)",     "Rohaniya",     "Sevapuri",     "Varanasi Cantt.",  "Varanasi North",   "Varanasi South",   "Shivpur"},
-        {"वाराणसी",       "पिंडरा",         "अजगर (एस सी)",     "रोहनिया",      "सेवापुरी",         "वाराणसी कैंट.",        "वाराणसी उत्तर",        "वाराणसी दक्षिण",       "शिवपुर"}
+            //  1               2                   3                4              5                  6                    7               8                   9                   10                  11                 12
+            {"Agra",        "Etmadpur",     "Agra Cantt. (SC)", "Agra South",   "Agra North",   "Agra Rural (SC)",  "Fatehpur Sikri",   "Kheragarh",        "Fatehabad",        "Bah"},
+            {"आगरा",        "एतमादपुर",       "आगरा कैंट. (एस सी)", "आगरा दक्षिण",   "आगरा उत्तर",      "आगरा ग्रामीण (एस सी)", "फतेहपुर सीकरी",     "खेरागढ़",             "फतेहाबाद",          "बाह"},
+            {"Aligarh",     "Khair (SC)",   "Barauli",          "Atrauli",      "Chharra",      "Koil",             "Aligarh",          "Iglas (SC)"},
+            {"अलीगढ़",       "खैर (एस सी)",   "बरौली",              "अतरौली",       "छर्रा",           "कोइल",             "अलीगढ़",           "इगलास (एस सी)"},
+            {"Allahabad",   "Phaphamau",    "Soraon (SC)",      "Phulpur",      "Pratappur",    "Handia",           "Meja",             "Karachhana",       "Allahabad West",   "Allahabad North", "Allahabad South",   "Bara (SC)",     "Koraon (SC)"},
+            {"इलाहाबाद",     "फाफामऊ",        "सोराअों (एस सी)",    "फूलपुर",         "प्रतापपुर",        "हंडिया",             "मेजा",             "करचना",            "इलाहाबाद पश्चिम",      "इलाहाबाद उत्तर",       "इलाहाबाद दक्षिण",      "बारा (एस सी)",     "कोरों (एस सी)"},
+            {"Ambedkar Nagar",     "Katehari",   "Tanda",          "Alapur (SC)",      "Jalalpur",      "Akbarpur"},
+            {"अंबेडकर नगर",           "कटेहरी",      "टांडा",            "अलापुर (एस सी)",    "जलालपुर",         "अकबरपुर"},
+            {"Amethi", "Tiloi" , "Salon (SC)", "Jagdishpur (SC)", "Gauriganj", "Amethi"},
+            {"अमेठी", "तिलोई", "सलोन (एस सी)", "जगदीशपुर (एस सी)", "गौरीगंज", "अमेठी"},
+            {"Amroha", "Dhanaura (SC)" , "Naugawan Sadat", "Amroha", "Hasanpur", "Garhmukteshwar"},
+            {"अमरोहा", "धनौरा (एस सी)" , "नौगावां सादात", "अमरोहा", "हसनपुर", "गढ़मुक्तेश्वर"},
+            {"Aonla", "Shekhupur", "Dataganj", "Faridpur (SC)", "Bithari Chainpur", "Aonla"},
+            {"आंवला", "शेखूपुर", "दातागंज", "फरीदपुर (एस सी)", "बिथरी चैनपुर", "आंवला"},
+            {"Azamgarh", "Gopalpur", "Sagri", "Mubarakpur", "Azamgarh", "Mehnagar (SC)"},
+            {"आजमगढ़", "गोपालपुर", "सगरी", "मुबारकपुर", "आजमगढ़", "मेहनगर (एस सी)"},
+            {"Badaun", "Gunnaur", "Bisauli (SC)", "Sahaswan", "Bilsi", "Badaun"},
+            {"बदायूं", "गुन्नौर", "बिसौली (एस सी)", "सहसवान", "बिल्सी", "बदायूं"},
+            {"Mirzapur",    "Chhanbey (SC)",     "Mirzapur",         "Majhawan",     "Chunar",       "Marihan"},
+            {"मिर्जापुर",       "छानबे (एस सी)",          "मिर्जापुर",            "मझवां",          "चुनार",         "मड़िहान"},
+            {"Varanasi",    "Pindra",       "Ajagara (SC)",     "Rohaniya",     "Sevapuri",     "Varanasi Cantt.",  "Varanasi North",   "Varanasi South",   "Shivpur"},
+            {"वाराणसी",       "पिंडरा",         "अजगर (एस सी)",     "रोहनिया",      "सेवापुरी",         "वाराणसी कैंट.",        "वाराणसी उत्तर",        "वाराणसी दक्षिण",       "शिवपुर"}
     };
 }
