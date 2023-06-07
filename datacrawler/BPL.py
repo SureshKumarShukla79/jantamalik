59ae3cc77fd8ae8078cb28abe121cec641d2feb9
Suresh Kumar Shukla <sureshkrshukla@gmail.com>
2023-06-07 18:11:15 +0530

    BPL captures cards and benefeciary both.

--- datacrawler/BPL.py
+++ datacrawler/BPL.py
@@ -18,37 +18,37 @@ list_count = 1
 
 janpadCount=len(driver.find_elements(By.XPATH, "//table/tbody/tr/td/a"))
 print(janpadCount)
-for i in range(1 ,janpadCount+1):
+for i in range(1, janpadCount+1):
     listOfJanpad =  driver.find_element(By.XPATH, "(//table/tbody/tr/td/a)["+str(janpadCount)+"]").text
     print(listOfJanpad)
     driver.find_element(By.XPATH, "(//table/tbody/tr/td/a)["+str(janpadCount)+"]").click()
     blockCount = len(driver.find_elements(By.XPATH, "//td[contains(text(),'ग्रामीण क्षेत्र')]/following::tr/td/a"))
     print(blockCount)
-    
-    for i in range(1,blockCount+1):
+    for i in range(1, blockCount+1):
+
         listOfBlock=driver.find_element(By.XPATH,"(//td[contains(text(),'ग्रामीण क्षेत्र')]/following::tr/td/a)["+str(blockCount)+"]").text
         driver.find_element(By.XPATH,"(//td[contains(text(),'ग्रामीण क्षेत्र')]/following::tr/td/a)["+str(blockCount)+"]").click()
         time.sleep(1)
         gramPanchayatCount = len(driver.find_elements(By.XPATH,"//tr/td/a"))
         print(gramPanchayatCount)
-        
-        for i in range(1,gramPanchayatCount+1):
+        for i in range(1, gramPanchayatCount+1):
             listOfPanchayat = driver.find_element(By.XPATH,"(//tr/td/a)["+str(gramPanchayatCount)+"]").text
 
             # time.sleep(1)
 
             numOfRation= driver.find_element(By.XPATH,"(//tr/td/a)["+str(gramPanchayatCount)+"]/following::td[5]").text
+            beneficiars = driver.find_element(By.XPATH,"(//tr/td/a)["+str(gramPanchayatCount)+"]/following::td[6]").text
+
             print("num of Ration Card: ",numOfRation)
             village_list.insert(list_count,
 
-                                {"District": listOfJanpad, "Block": listOfBlock, "Panchayat": listOfPanchayat,"Number Of Ration": numOfRation})
+                                {"District": listOfJanpad, "Block": listOfBlock, "Panchayat": listOfPanchayat,"Number Of Ration": numOfRation,"Beneficiars":beneficiars})
             list_count = list_count + 1
 
             gramPanchayatCount=gramPanchayatCount-1
             # time.sleep(1) #for back to the previous page
         driver.back()
         blockCount=blockCount-1
-        
     driver.back()
     janpadCount=janpadCount-1
 
